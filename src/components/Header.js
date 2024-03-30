c0fdd80bb434e95bee889971a8248ca2e3ae24a0
Mathieu <mathieu.barbey18@gmail.com>
2024-03-30 16:57:46 +0100

    navbar with active link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { Container, Nav, Navbar } from "react-bootstrap"
 import "../index.css"
+import { useLocation } from "react-router-dom"
 
-function Header() {
+function Header(props) {
+    const location = useLocation()
+
     return (
-        <Navbar expand="lg" bg="headerBackground" data-bs-theme="dark">
+        <Navbar activeKey={location.pathname} expand="lg" bg="headerBackground" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand className="text-start"><strong style={{fontSize: "1.5em"}}>John Doe</strong></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav" >
-                    <Nav>
+                    <Nav activeKey={location.pathname}>
                         <Nav.Link href="/">Accueil</Nav.Link>
                         <Nav.Link>Service</Nav.Link>
                         <Nav.Link>Realisations</Nav.Link>
